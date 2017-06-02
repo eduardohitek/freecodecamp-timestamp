f48345e39834ad04a07ee92bb1e2ac014fdb89bc
Eduardo Hitek <eduardohitek@gmail.com>
2017-06-02 13:08:00 +0000

    Finished

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require("express")
 const app = express()
+const moment = require("moment")
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<h3>Example usage:</h3>'+
+            '<code>https://freecodecamp-api-projects-eduardohitek.c9users.io/December%2015,%202015</code><br>'+
+            '<code>https://freecodecamp-api-projects-eduardohitek.c9users.io/1450137600</code>'+
+            '<h3>Example output:</h3>'+
+            '<code>'+
+                '{'+
+                  '"unix": 1450137600,'+
+                  '"natural": "December 15, 2015"'+
+                '}'+
+            '</code>')
 })
+
+app.get('/:param', function (req, res) {
+  const param = req.params.param
+  var myDate;
+  
+  if(isNaN(param)){
+    myDate = moment(param, "MMMM D, YYYY")
+  }
+  else{
+    myDate = moment(param, "X")
+  }
+  console.log(param)
+  console.log(myDate)
+  if(myDate.isValid()){
+    res.json({unix: myDate.format("X"), natural: myDate.format("MMMM D, YYYY")})
+  }
+    else{
+      res.json({unix: null, natural: null})
+    }
+
+})
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
